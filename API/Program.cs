5e33ad827a143c3eccd5b738062de6921bac6f5b
Ivan Misarosh <ivan.misarosh05@gmail.com>
2024-11-18 14:32:17 +0200

    added repositories and sevices to use them with DI

--- API/Program.cs
+++ API/Program.cs
@@ -20,10 +20,20 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IRepository<ICustomer>, CustomerRepository>();
 builder.Services.AddScoped<IRepository<ICar>, CarRepository>();
 builder.Services.AddScoped<IRepository<IVisit>, VisitRepository>();
+builder.Services.AddScoped<IRepository<IVisitStatus>, VisitStatusRepository>();
+builder.Services.AddScoped<IRepository<IPaymentStatus>, PaymentStatusRepository>();
+builder.Services.AddScoped<IRepository<IEmployee>, EmployeeRepository>();
+builder.Services.AddScoped<IRepository<ICarModel>, CarModelRepository>();
+builder.Services.AddScoped<IRepository<IColor>, ColorRepository>();
 
 builder.Services.AddScoped<IService<CustomerDTO>, CustomerService>();
 builder.Services.AddScoped<IService<CarDTO>, CarService>();
 builder.Services.AddScoped<IService<VisitDTO>, BLL.Services.VisitService>();
+builder.Services.AddScoped<IService<VisitStatusDTO>, BLL.Services.VisitStatusService>();
+builder.Services.AddScoped<IService<PaymentStatusDTO>, BLL.Services.PaymentStatusService>();
+builder.Services.AddScoped<IService<EmployeeDTO>, BLL.Services.EmployeeService>();
+builder.Services.AddScoped<IService<CarModelDTO>, BLL.Services.CarModelService>();
+builder.Services.AddScoped<IService<ColorDTO>, BLL.Services.ColorService>();
 
 builder.Services.AddAutoMapper(typeof(MapperProfile));
 //builder.Services.AddAutoMapper(typeof(MapperProfile));
