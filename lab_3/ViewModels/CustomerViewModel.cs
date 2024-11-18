471985d659c36c9dd9daeb01a0e271df03f58bea
Ivan Misarosh <ivan.misarosh05@gmail.com>
2024-11-18 12:42:17 +0200

    updated customer viewmodel to use api calls

--- lab_3/ViewModels/CustomerViewModel.cs
+++ lab_3/ViewModels/CustomerViewModel.cs
@@ -1,26 +1,23 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Input;
 using Abstraction;
-using Abstraction.ModelInterfaces;
 using lab_3.Command;
 using lab_3.InfoWindows;
 using Newtonsoft.Json;
+using System.Net.Http;
 using Abstraction.DTOs;
+using System.Text;
 
 namespace lab_3.ViewModels
 {
     public class CustomerViewModel : BaseViewModel
     {
-        private readonly IRepository<ICustomer> _customerRepository;
-        private ICustomer _selectedCustomer;
+        private CustomerDTO _selectedCustomer;
         private CustomerInfoWindow _customerInfoWindow;
 
-        private ObservableCollection<ICustomer> _customers;
-        public ObservableCollection<ICustomer> Customers
+        private ObservableCollection<CustomerDTO> _customers;
+        public ObservableCollection<CustomerDTO> Customers
         {
             get => _customers;
             set
@@ -29,9 +26,8 @@ namespace lab_3.ViewModels
                 OnPropertyChanged();
             }
         }
-        public IRepositoryFactory RepositoryFactory { get; }
 
-        public ICustomer SelectedCustomer
+        public CustomerDTO SelectedCustomer
         {
             get => _selectedCustomer;
             set
@@ -53,8 +49,6 @@ namespace lab_3.ViewModels
 
         public CustomerViewModel(IRepositoryFactory repositoryFactory)
         {
-            RepositoryFactory = repositoryFactory ?? throw new ArgumentNullException(nameof(repositoryFactory));
-            _customerRepository = repositoryFactory.GetRepository<ICustomer>();
 
             // Initialize commands
             AddCommand = new RelayCommand(AddCustomer);
@@ -64,48 +58,44 @@ namespace lab_3.ViewModels
             UpdateCommand = new RelayCommand(UpdateCustomer);
 
             // Load initial data
-            InitializeCustomers();
+            LoadCustomers();
         }
 
-        private async void InitializeCustomers()
+        private async void LoadCustomers()
         {
             var response = await HttpClient.GetStringAsync($"{ServiceUrl}api/Customer");
             var customers = JsonConvert.DeserializeObject<List<CustomerDTO>>(response);
             if (customers is null)
                 return;
-            Customers = new ObservableCollection<ICustomer>(customers);
+            Customers = new ObservableCollection<CustomerDTO>(customers);
 
         }
 
         public void AddCustomer(object parameter)
         {
-            SelectedCustomer = RepositoryFactory.CreateCustomer();
+            SelectedCustomer = new CustomerDTO();
             OpenCustomerInfoWindow();
         }
 
-        public void SaveCustomer(object parameter)
+        public async void SaveCustomer(object parameter)
         {
             if (SelectedCustomer == null) return;
 
-            try
+            var json = JsonConvert.SerializeObject(SelectedCustomer);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            
+            
+            if (SelectedCustomer.CustomerID == 0)
             {
-                if (SelectedCustomer.CustomerID == 0)
-                {
-                    _customerRepository.Add(SelectedCustomer);
-                }
-                else
-                {
-                    _customerRepository.Update(SelectedCustomer);
-                }
-                _customerRepository.SaveChanges();
-                UpdateCustomerList();
-                _customerInfoWindow?.Close();
+                var response = await HttpClient.PostAsync($"{ServiceUrl}api/Customer", content);
             }
-            catch (Exception ex)
+            else
             {
-                MessageBox.Show($"Error saving customer: {ex.Message}", "Error",
-                    MessageBoxButton.OK, MessageBoxImage.Error);
+                var response = await HttpClient.PutAsync($"{ServiceUrl}api/Customer/{SelectedCustomer.CustomerID}", content);
             }
+            LoadCustomers();
+            _customerInfoWindow?.Close();
+            
         }
 
         private void OpenCustomerInfoWindow()
@@ -129,19 +119,12 @@ namespace lab_3.ViewModels
             var result = MessageBox.Show("Are you sure you want to delete this customer?",
                 "Confirm Delete", MessageBoxButton.YesNo, MessageBoxImage.Question);
 
+
             if (result == MessageBoxResult.Yes)
             {
-                try
-                {
-                    _customerRepository.Delete(SelectedCustomer);
-                    _customerRepository.SaveChanges();
-                    UpdateCustomerList();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"Error deleting customer: {ex.Message}", "Error",
-                        MessageBoxButton.OK, MessageBoxImage.Error);
-                }
+                var customerId = SelectedCustomer.CustomerID;
+                var response = HttpClient.DeleteAsync($"{ServiceUrl}api/Customer/{customerId}");
+                LoadCustomers();
             }
         }
 
@@ -158,28 +141,5 @@ namespace lab_3.ViewModels
             _customerInfoWindow?.Close();
         }
 
-        private void UpdateCustomerList()
-        {
-            if (Customers == null)
-            {
-                Customers = new ObservableCollection<ICustomer>();
-            }
-
-            try
-            {
-                Customers.Clear();
-                var customers = _customerRepository.GetAll();
-                foreach (var customer in customers)
-                {
-                    Customers.Add(customer);
-                }
-                System.Diagnostics.Debug.WriteLine($"Updated customer list: {Customers.Count} customers");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error updating customer list: {ex.Message}", "Error",
-                    MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
     }
 }
